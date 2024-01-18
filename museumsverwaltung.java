852d15a865ad2c62e3e0e93622306540b4b86b9d
JoelBkr <135157970+JoelBkr@users.noreply.github.com>
2024-01-18 08:50:20 +0100

    updated methods to lend

--- museumsverwaltung.java
+++ museumsverwaltung.java
@@ -12,15 +12,15 @@ public class museumsverwaltung {
         List exponate = new List<Exponat>();
     }
 
-    public void ausleihen (name) {
-
+    public void ausleihen (Exponat name) {
+        name.aendereAusgeliehen();
     }
 
-    public void ausstellen (name) {
-        
+    public void ausstellen (Exponat name) {
+        name.aendereAusstellung();
     }
 
-    public void einlagern (name) {
-        
+    public void einlagern (Exponat name) {
+        name.aendereLager();
     }
 }
